e465e187bb9382e36123b09e028219eda11913ba
Phillip Verheyden <pverheyden@gmail.com>
2019-02-06 19:41:52 -0600

    Multi-module support
    Searches for the .git directory up from the current directory to install hooks into
    
    Fixes #3

--- src/main/java/org/sandbox/GitHookInstallMojo.java
+++ src/main/java/org/sandbox/GitHookInstallMojo.java
@@ -9,34 +9,41 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 @Mojo(name = "install", defaultPhase = LifecyclePhase.INITIALIZE)
 public final class GitHookInstallMojo extends AbstractMojo {
 
     private static final String SHEBANG = "#!/bin/sh";
-    private static final Path HOOK_DIR_PATH = Paths.get(".git/hooks");
 
     @Parameter
     private Map<String, String> hooks;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        if (!HOOK_DIR_PATH.toFile().exists()) {
+        Path hooksDir;
+        try {
+            hooksDir = getHooksDir();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Exception searching for .git/hooks", e);
+        }
+
+        if (hooksDir == null) {
             throw new MojoExecutionException(
                     "Not a git repository, could not find .git/hooks directory");
         }
+
         for (Map.Entry<String, String> hook : hooks.entrySet()) {
             String hookName = hook.getKey();
             String finalScript = SHEBANG + '\n' + hook.getValue();
             try {
-                File created = Files.write(HOOK_DIR_PATH.resolve(hookName), finalScript.getBytes(),
+                File created = Files.write(hooksDir.resolve(hookName), finalScript.getBytes(),
                         CREATE, TRUNCATE_EXISTING).toFile();
                 boolean successul = created.setExecutable(true, true)
                         && created.setReadable(true, true)
@@ -50,6 +57,15 @@ public final class GitHookInstallMojo extends AbstractMojo {
                 throw new MojoExecutionException("Could not write hook with name: " + hookName, e);
             }
         }
+    }
+
+    private Path getHooksDir() throws IOException {
+        File gitMetadataDir = new FileRepositoryBuilder()
+                .findGitDir()
+                .build()
+                .getDirectory();
+
+        return gitMetadataDir == null ? null : gitMetadataDir.toPath().resolve("hooks");
     }
 
 }
